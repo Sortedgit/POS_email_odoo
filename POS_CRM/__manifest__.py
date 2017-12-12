bab4c34a083632f02c5597cab124178288850b5b
Sortedgit <34474703+Sortedgit@users.noreply.github.com>
2017-12-12 22:21:49 +0530

    Create __manifest__.py

---
+++ POS_CRM/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{   
+    'name': "POS Email",
+    'summary': "Add Mass Mailing to POS",
+    'description':"This will enhance your POS by enabling you to communicate with your POS customers using CRM mass mailing for marketing",
+    'author': "SortedPixel",
+    'website': "http://www.sortedpixel.com",
+    'version': '0.1',
+    'license': 'OEEL-1',
+    'depends': ['base','point_of_sale'],
+    'images': ['images/main_screenshot.jpeg'],
+    'data': ['views/POSCRM_views.xml'],
+}
